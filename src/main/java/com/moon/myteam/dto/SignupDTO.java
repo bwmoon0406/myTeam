8f553e753b9a0f54348899ffe463ea5a30490f27
moon <bwmoon0406@gamil.com>
2024-04-08 15:36:26 +0900

    4차 수정

--- src/main/java/com/moon/myteam/dto/SignupDTO.java
+++ src/main/java/com/moon/myteam/dto/SignupDTO.java
@@ -1,2 +1,11 @@
-package com.moon.myteam.dto;public class SignupDTO {
+package com.moon.myteam.dto;
+
+import lombok.Data;
+
+@Data
+public class SignupDTO {
+    private String id;
+    private String pw;
+    private String teamName;
+    private String email;
 }
